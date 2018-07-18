899ffe62970971c511295aefcea492628f7b3a4e
Rob Brazier <RobBrazier@users.noreply.github.com>
2018-07-18 22:01:21 +0100

    remove depends_on and separate build and push

--- .ci/pipeline.sh
+++ .ci/pipeline.sh
@@ -3,16 +3,24 @@
 set -eu
 
 declare -a PHP_VERSIONS=(5.6 7.0 7.1 7.2 latest)
+DOCKER_INDEX="index.docker.io/robbrazier/php"
 
 echo "steps:"
-echo "  - label: ':php: builder'"
+echo "  - label: 'Build :php: builder'"
 echo "    plugins:"
 echo "      docker-compose#v2.5.0:"
 echo "        build: builder"
 echo "  - wait"
-for phpver in ${PHP_VERSIONS[@]}; do
-    echo "  - label: ':php: $phpver'"
-    echo "    plugins:"
-    echo "      docker-compose#v2.5.0:"
-    echo "        build: php${phpver//\./}"
-done
+echo "  - label: 'Push :php: builder'"
+echo "    plugins:"
+echo "      docker-compose#v2.5.0:"
+echo "        push:"
+echo "          - builder:$DOCKER_INDEX:builder"
+
+# echo "  - wait"
+# for phpver in ${PHP_VERSIONS[@]}; do
+#     echo "  - label: ':php: $phpver'"
+#     echo "    plugins:"
+#     echo "      docker-compose#v2.5.0:"
+#     echo "        build: php${phpver//\./}"
+# done
