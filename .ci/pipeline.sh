1939d99bf2fdab7f9c10825c2c567ce3b5c47b91
Rob Brazier <RobBrazier@users.noreply.github.com>
2018-07-18 22:24:11 +0100

    test php5.5 push from buildkite

--- .ci/pipeline.sh
+++ .ci/pipeline.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-declare -a PHP_VERSIONS=(5.5 5.6 7.0 7.1 7.2 latest)
+declare -a PHP_VERSIONS=(5.5)# 5.6 7.0 7.1 7.2 latest)
 DOCKER_INDEX="index.docker.io/robbrazier/php"
 
 echo "steps:"
@@ -22,8 +22,20 @@ echo "        push:"
 echo "          - builder:$DOCKER_INDEX:builder"
 echo "  - wait"
 for phpver in ${PHP_VERSIONS[@]}; do
-    echo "  - label: ':php: $phpver'"
+    echo "  - label: 'Build :php: $phpver'"
     echo "    plugins:"
     echo "      docker-compose#v2.5.0:"
     echo "        build: php${phpver//\./}"
+done
+echo "  - wait"
+for phpver in ${PHP_VERSIONS[@]}; do
+    SERVICE_NAME="php${phpver//\./}"
+    echo "  - label: 'Push :php: $phpver'"
+    echo "    plugins:"
+    echo "      docker-login#v2.0.0:"
+    echo "        username: robbrazier"
+    echo "        password-env: DOCKER_LOGIN_PASSWORD"
+    echo "      docker-compose#v2.5.0:"
+    echo "        push:"
+    echo "          - $SERVICE_NAME:$DOCKER_INDEX:$phpver-test"
 done
