9bb073b0663edf4ed350ef71a607ebb832a44afd
Rob Brazier <RobBrazier@users.noreply.github.com>
2018-07-19 21:09:41 +0100

    add git

--- .common/setup.sh
+++ .common/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
-apk add --no-cache bash gosu --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/
+apk add --no-cache bash gosu git --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/
 mkdir -p $COMPOSER_HOME /var/app
 chown -R nobody:nobody $COMPOSER_HOME /var/app
 gosu nobody composer global require hirak/prestissimo --no-progress
