707418c5769d49685d4aed7f3650df8cead2a1d6
Rob Brazier <RobBrazier@users.noreply.github.com>
2018-07-19 20:10:01 +0100

    remove builder

--- .common/setup.sh
+++ .common/setup.sh
@@ -4,3 +4,4 @@ curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --fil
 apk add --no-cache bash gosu --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/
 mkdir -p $COMPOSER_HOME /var/app
 chown -R nobody:nobody $COMPOSER_HOME /var/app
+gosu nobody composer global require hirak/prestissimo --no-progress
