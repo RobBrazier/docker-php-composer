42b5c2f9b3545a95f26c4bc76639f680164249ce
Rob Brazier <RobBrazier@users.noreply.github.com>
2019-08-24 21:51:17 +0100

    Configure SemaphoreCI v2 to build the images (#3)

--- .common/setup.sh
+++ .common/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
-apk add --no-cache bash gosu git --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/
+composer self-update
+apk add --no-cache bash gosu git apk-tools --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/
 mkdir -p $COMPOSER_HOME /var/app
 chown -R nobody:nobody $COMPOSER_HOME /var/app
 gosu nobody composer global require hirak/prestissimo --no-progress
